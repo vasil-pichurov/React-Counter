1d9c68fa5892da9a3ddecd89c657c1f34b68707a
vasilpichurov <vasil.pichurov@gmail.com>
2025-12-03 16:03:32 +0200

    update config and html for production

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/React-Counter/', // must match repo name
+  base: '/React-Counter/', // must match repo name exactly
   plugins: [react()],
 })

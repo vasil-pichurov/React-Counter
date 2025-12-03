dfdb924a0ee44299275e2caae40b56932e5f9851
vasilpichurov <vasil.pichurov@gmail.com>
2025-12-03 16:06:48 +0200

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/React-Counter/', // must match repo name exactly
+  base: '/React-Counter/', // MUST match your GitHub repo name
   plugins: [react()],
 })
